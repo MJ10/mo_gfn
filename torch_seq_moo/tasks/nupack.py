cee573cfaa7f02ef048f29f4f96b341549173017
alexhernandezgarcia <alexhdezgarcia@gmail.com>
2022-09-18 18:20:32 -0400

    num_at

--- torch_seq_moo/tasks/nupack.py
+++ torch_seq_moo/tasks/nupack.py
@@ -121,6 +121,9 @@ class NupackTask(BaseTask):
             beta_length = 0.05
             explength = np.asarray([np.exp(-1.0 * beta_length * len(seq)) for seq in sequences])
             dict_return.update({"explength": explength})
+        if "num_at" in objectives:
+            num_at = np.asarray([seq.count("A") + seq.count("T") for seq in sequences])
+            dict_return.update({"num_at": -num_at})
 
         if "open loop" in objectives:
             biggest_loop = np.zeros(len(ssStrings))
